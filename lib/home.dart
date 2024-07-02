121dc608b1bc4a88f24189dc7925cea192bdf56d
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-07-02 12:36:59 +0700

    cara membuat appbar

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,13 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("Home Test"),
+        leading: Icon(Icons.arrow_back_ios),
+        actions: [Icon(Icons.notifications_active), Icon(Icons.history)],
+      ),
+    );
   }
 }
