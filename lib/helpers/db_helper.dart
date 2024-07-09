ee18baf3e2f33cad95676cb06ddd27ae8c53a8d6
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-07-09 13:27:46 +0700

    create database beserta dengan table dan column

---
+++ lib/helpers/db_helper.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DbHelper {
+  static DbHelper? _dbHelper;
+  static Database? _database;
+
+  DbHelper._createObject();
+
+  factory DbHelper() {
+    if (_dbHelper == null) {
+      _dbHelper = DbHelper._createObject();
+    }
+    return _dbHelper!;
+  }
+
+  Future<Database> initDB() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path + "absensi.db";
+
+    var todoDatabase = openDatabase(path, version: 1, onCreate: _createDB);
+
+    return todoDatabase;
+  }
+
+  void _createDB(Database database, int version) async {
+    await database.execute("""
+      CREATE TABLE absensi (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nama TEXT,
+      status_hadir TEXT
+      )
+    """);
+  }
+
+  Future<Database> get database async {
+    if (_database == null) {
+      _database = await initDB();
+    }
+
+    return _database!;
+  }
+}
