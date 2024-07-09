0eb5ea268efcf7a6b6b783c46a54cca32eaad715
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-07-09 13:43:41 +0700

    membuat models absensi

---
+++ lib/models/absensi.dart
@@ -0,0 +1,35 @@
+class Absensi {
+  int? _id;
+  String? _nama;
+  // ignore: non_constant_identifier_names
+  String? _status_hadir;
+
+  Absensi(this._id, this._nama, this._status_hadir);
+
+  Absensi.fromMap(Map<String, dynamic> map) {
+    this._id = map["id"];
+    this._nama = map["nama"];
+    this._status_hadir = map["status_hadir"];
+  }
+
+  int get id => _id!;
+  String get nama => _nama!;
+  String get status_hadir => _status_hadir!;
+
+  set nama(String value) {
+    _nama = value;
+  }
+
+  set status_hadir(String value) {
+    _status_hadir = value;
+  }
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = Map<String, dynamic>();
+    map['id'] = this._id;
+    map['nama'] = nama;
+    map['status_hadir'] = status_hadir;
+
+    return map;
+  }
+}
